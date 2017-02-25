d2f86000d40aa403e719e2608a4e0befa52a5e3d
Mizuku <mizuku.ga@gmail.com>
2017-02-25 16:41:23 +0900

    first commit

---
+++ SampleOxyPlot/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleOxyPlot
+{
+    class MainWindowViewModel
+    {
+        public ObservableCollection<PlotItem> PlotItems { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            PlotItems = new ObservableCollection<PlotItem>(Enumerable.Range(0, 4)
+                .Select(i => new PlotItem()
+                {
+                    // oxyPlot のグラフの値に binding できるのは数値だけっぽい
+                    Pos = i,
+                    Date = new DateTime(2017, 3, 1 + i * 2),
+                    BloodPressure = 100 + i * 4,
+                    BodyTemperature = 36.0 + i * .5,
+                    Pulse = 80 - i,
+                    Breathing = 25 - i,
+                }));
+
+            // DataFieldX と DataFieldY を指定しない場合 DataPoint のコレクションにする、みたいだけど動かないような？使い方が間違ってるだけ？
+            //BloodPressures = PlotItems.Select((e, i) => new DataPoint(i, e.BloodPressure)).ToList();
+            //BodyTemperatures = PlotItems.Select((e, i) => new DataPoint(i, e.BodyTemperature)).ToList();
+            //Pulses = PlotItems.Select((e, i) => new DataPoint(i, e.Pulse)).ToList();
+            //Breathings = PlotItems.Select((e, i) => new DataPoint(i, e.Breathing)).ToList();
+        }
+    }
+}
---
+++ SampleOxyPlot/Plot/PlotItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleOxyPlot
+{
+    class PlotItem
+    {
+        [DisplayName("日付")]
+        public DateTime Date { get; set; }
+
+        public int Pos { get; set; }
+
+        [DisplayName("血圧")]
+        public double BloodPressure { get; set; }
+
+        [DisplayName("体温")]
+        public double BodyTemperature { get; set; }
+
+        [DisplayName("脈拍")]
+        public double Pulse { get; set; }
+
+        [DisplayName("呼吸数")]
+        public double Breathing { get; set; }
+
+    }
+}
